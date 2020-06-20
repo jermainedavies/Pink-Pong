c8dfee8f1ba3379a13390584ceba2d5304cf2787
Jermaine Simon Davies <jermainedavies@outlook.com>
2020-06-20 09:32:22 +0100

    Add files via upload

---
+++ Pink Pong Windows.py
@@ -0,0 +1,131 @@
+#Pong game written in Python by Jermaine Simon Davies
+
+import turtle
+import winsound
+
+wn = turtle.Screen()
+wn.title("Pink Pong by @jermainedavies")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+#Scorekeeping
+score_1 = 0
+score_2 = 0
+
+
+#Padde 1
+paddle_1 = turtle.Turtle()
+paddle_1.speed(0)
+paddle_1.shape("square")
+paddle_1.color("pink")
+paddle_1.shapesize(stretch_wid=5, stretch_len=1)
+paddle_1.penup()
+paddle_1.goto(-350, 0)
+
+
+
+#Paddle 2
+paddle_2 = turtle.Turtle()
+paddle_2.speed(0)
+paddle_2.shape("square")
+paddle_2.color("pink")
+paddle_2.shapesize(stretch_wid=5, stretch_len=1)
+paddle_2.penup()
+paddle_2.goto(350, 0)
+
+#Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("pink")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.5
+ball.dy = 0.5
+
+#Score system (pen)
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("pink")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player 1: 0 Player 2: 0", align="center", font= ("Helvetica", 24, "bold"))
+
+#Functions
+def paddle_1_up():
+    y = paddle_1.ycor()
+    y += 20
+    paddle_1.sety(y)
+
+def paddle_1_down():
+    y = paddle_1.ycor()
+    y -= 20
+    paddle_1.sety(y)
+
+def paddle_2_up():
+    y = paddle_2.ycor()
+    y += 20
+    paddle_2.sety(y)
+
+def paddle_2_down():
+    y = paddle_2.ycor()
+    y -= 20
+    paddle_2.sety(y)
+
+#Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_1_up, "w")
+wn.onkeypress(paddle_1_down, "s")
+wn.onkeypress(paddle_2_up, "Up")
+wn.onkeypress(paddle_2_down, "Down")
+
+#main game loop
+while True:
+    wn.update()
+
+    #Ball movement
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        winsound.PlaySound("ball_collide.wav", winsound.SND_ASYNC)
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        winsound.PlaySound("ball_collide.wav", winsound.SND_ASYNC)
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_1 += 1
+        pen.clear()
+        pen.write("Player 1: {} Player 2: {} ".format(score_1, score_2), align="center", font= ("Helvetica", 24, "bold"))
+        winsound.PlaySound("point_won.wav", winsound.SND_ASYNC)
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_2 += 1
+        pen.clear()
+        pen.write("Player 1: {} Player 2: {} ".format(score_1, score_2), align="center", font= ("Helvetica", 24, "bold"))
+        winsound.PlaySound("point_won.wav", winsound.SND_ASYNC)
+
+    #Paddle and ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_2.ycor() + 40 and ball.ycor() > paddle_2.ycor() -40):
+       ball.setx(340)
+       ball.dx *= -1
+       winsound.PlaySound("ball_collide.wav", winsound.SND_ASYNC)
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_1.ycor() + 40 and ball.ycor() > paddle_1.ycor() -40):
+       ball.setx(-340)
+       ball.dx *= -1
+       winsound.PlaySound("ball_collide.wav", winsound.SND_ASYNC)
+       
+
+#find out how to get paddles to move simultaneously to allow for a true multiplayer experience
